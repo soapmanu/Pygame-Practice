ce0b7052e1f1ac09e377f0f5807c9b27d8eaa762
soapmanu <joseemanueljimenez@gmail.com>
2023-03-18 12:51:41 -0300

    setup for new version graphics, sounds

--- Zeldalike_Game/version-0/player.py
+++ Zeldalike_Game/version-0/player.py
@@ -4,7 +4,7 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self,pos,groups,obstacle_sprites):
         super().__init__(groups)
-        self.image = pygame.image.load('Zeldalike_Game/version-0/graphics/test/player.png').convert_alpha()
+        self.image = pygame.image.load('graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0,-26)
         
