316789c6a54d51f5c89f17da22edd4cd53d1265b
soapmanu <joseemanueljimenez@gmail.com>
2023-03-19 16:11:44 -0300

    ..

--- Zeldalike_Game/version-1/main.py
+++ Zeldalike_Game/version-1/main.py
@@ -1,15 +1,18 @@
-import pygame,sys
+import pygame, sys
 from settings import *
+from debug import debug
+from level import Level
 
 class Game:
     def __init__(self):
-        
         # Setup
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
-        pygame.display.set_caption('Zeldalike')
+        pygame.display.set_caption('Zeldalike Game')
         self.clock = pygame.time.Clock()
-    
+        
+        self.level = Level()
+        
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -17,10 +20,12 @@ class Game:
                     pygame.quit()
                     sys.exit()
                     
-                self.screen.fill('black')
-                pygame.display.update()
-                self.clock.tick(FPS)
-            
+            self.screen.fill('dark green')
+            self.level.run()
+            #debug('Hello')
+            pygame.display.update()
+            self.clock.tick(FPS)
+    
 if __name__ == '__main__':
     game = Game()
     game.run()
--- Zeldalike_Game/version-1/tile.py
+++ Zeldalike_Game/version-1/tile.py
@@ -4,6 +4,6 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
         super().__init__(groups)
-        self.image = pygame.image.load('Zeldalike_Game/version-0/graphics/test/rock.png').convert_alpha()
+        self.image = pygame.image.load('Zeldalike_Game/version-1/graphics/test/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0,-10)
